4a31aefd6af5e6acb0567c61f08f01d70a8cbf6f
fernandorvarela <31898802+fernandorvarela@users.noreply.github.com>
2017-09-13 18:01:23 -0300

    Evaluacion
    Ejercicio 5

---
+++ EjercicioCinco.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocinco;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioCinco {
+
+    /**
+     * @param args the command line arguments
+     * Se necesita pedir dos numeros y sumarlos, mostrar el resultado
+     */
+    public static void main(String[] args) {
+        int NumeroUno;
+        int NumeroDos;
+        int Suma;
+        Scanner 
+        System.out.println("estoy en el ejercicio 5");
+        
+        System.out.println("El resultado es: " + (Suma.in));
+        
+        
+    }
+    
+}
