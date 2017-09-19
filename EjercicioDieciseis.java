72b5f409f1d37ff2da22b89f88c1a2b5ab00f423
fernandorvarela <31898802+fernandorvarela@users.noreply.github.com>
2017-09-19 18:04:39 -0300

    Ejercicio16

---
+++ EjercicioDieciseis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciodieciseis;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioDieciseis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 16 Se ingresa el importe de un pasaje de avion y el mes de viaje, si es enero se le descuenta un 10% por temporada baja.
+    }
+    
+}
