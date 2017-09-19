10ff702cfd4695eecad64c1b5c974fdc800948a0
fernandorvarela <31898802+fernandorvarela@users.noreply.github.com>
2017-09-19 18:04:18 -0300

    Ejercicio15

---
+++ EjercicioQuince.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioquince;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioQuince {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // //15 Se ingresa un importe si supera los 100 pesos, se le suma un 23%. Si es menor a 100 pesos se le descuenta el 50%.
+    }
+    
+}
