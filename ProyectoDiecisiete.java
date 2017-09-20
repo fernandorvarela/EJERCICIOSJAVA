e47e4dfb8c2a40aea15755e00347aec8dfde340e
fernandorvarela <31898802+fernandorvarela@users.noreply.github.com>
2017-09-20 17:42:54 -0300

    EjercicioDiecisiete

---
+++ ProyectoDiecisiete.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodiecisiete;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProyectoDiecisiete {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Se pide una edad y se informa si es menor, adolescente o mayor.
+        System.out.println("hola 17");
+
+        Integer edad;
+        //
+        //
+        edad = 99;
+
+        if (edad > 17) {
+            System.out.println("es mayor");
+        } else if (edad < 13) {
+            System.out.println("es niño");
+        } else {
+
+            System.out.println("es adolescente");
+        }
+//        if (edad>12 && edad<18) {
+//            System.out.println("es adolescente");
+//            
+//        }
+
+    }
+
+}
+
+    
+
