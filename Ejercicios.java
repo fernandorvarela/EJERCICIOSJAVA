b18d696e36259eee30dcdcaf65d6fdac3da3dbab
fernandorvarela <31898802+fernandorvarela@users.noreply.github.com>
2017-10-03 14:26:30 -0300

    Ejercicios de la clase

--- Ejercicios.java
+++ Ejercicios.java
@@ -15,23 +15,23 @@ public class Ejercicios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /* 13- Se ingresa un color y una localidad, si es avellaneda o Lanús y el color es rojo, mostrar el mensaje “sos de independiente”.
-14- Se pide el sexo y el nombre, si es “f” de femenino, se muestra el mensaje” feliz dia XXXXX”, donde XXXX es el nombre.
-15-Se pide el nombre, el sexo y la localidad. Si se llama José o maría y es de sexo masculino, viviendo en morón, se muestra el mensaje “sos de morón”.
-16-Se debe ingresar un edad  e  informar si es mayor de edad o no.
-17-Se pide una edad y se informa si es niño adolescente o mayor de edad
-18- Se pide una clave  y si coincide de muestra el mensaje “Bienvenido”  de lo contrario mostrar el mensaje “clave incorrecta.
-19- se ingresa un importe, si supera los 100 pesos se  le suma un impuesto del 23%, si no se les descuenta el 50%
-20- Se ingresa un importe del pasaje de avión y el mes de viaje, si el mes es enero, se les descuenta un 10% por temporada baja.
-22- se ingresa una nota, si es 10 mostrar excelente, si esta entre 9 y 4 mostrar aprobó , de lo contrario mostrar “ la próxima será” .
-Ferretería Ferrete
-Debemos hacer una serie de aplicaciones para este negocio
-	23- Debemos pedir el ancho y el largo de un terreno e informar cuantos metros de alambre necesitamos para dar tres (3) vueltas de alambre.
-24- Debemos pedir el ancho, el largo de un terreno y la cantidad de hilos de alambre e informar cuantos metros de alambre necesitamos para completar las vueltas de alambre.
-	25-Sabiendo que necesito 2 bolsas de cal y 3 de cemento para una pared de 1 metro cuadrado, se pide el alto y ancho y se informa la cantidad de cada material que necesito.
-	26- de pide una temperatura y se pide si se quiere pasar Celsius o Fahrenheit, hacer la cuenta y mostrar el resultado.
-	27- para una pileta se necesita saber la superficie, se debe pedir que tipo de pileta tiene (cuadrada o redonda) de ser redonda, pedir el radio y si es cuadrada pedir el largo y el ancho, mostrar la superficie de la pileta.*/
-
+        /* 1- Se debe mostrar el siguiente mensaje “esto funciona de maravilla”.
+2- Se debe pedir el nombre y luego mostrarlo.
+3- Se debe pedir el nombre y el apellido, luego mostrarlo en el siguiente mensaje “su nombre
+es XXXXX y XXXXXX es su apellido”.
+4- Al ingresar la edad y el nombre mostrar el siguiente mensaje: “ Usted se llama “ XXXXXX y
+tiene XX años de edad”.
+5- Se necesita pedir dos números y sumarlos, mostrar el resultado.
+6- Se piden tres números e informar el promedio.
+7- Se pide la base de un cuadrado y se informa el perímetro
+8- Se pide el largo y ancho de un terreno u se pide la superficie del mismo.
+9- Se ingresa la cantidad y precio de un producto y se informa el total a pagar.
+10- Se ingresa la cantidad de ganadores da la lotería nacional y el monto total del premio,
+informar cuánto dinero le corresponde a cada uno.
+11- Se pide el valor de la hora de trabajo de un empleado y la cantidad de horas trabajadas,
+informar cuanto le corresponde de sueldo, restándole el 15% de aportes.
+12- Se ingresa el importe de un producto importado y se le debe agregar el 35 % de impuestos
+internos.*/
     }
     
 }
